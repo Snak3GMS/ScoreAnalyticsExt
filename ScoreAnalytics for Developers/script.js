78af937a00152d77ac95c74f81809657ff891d77
Snak3GMS <36984325+Snak3GMS@users.noreply.github.com>
2021-04-23 01:21:37 +0300

    Add files via upload

--- ScoreAnalytics for Developers/script.js
+++ ScoreAnalytics for Developers/script.js
@@ -6,6 +6,7 @@ class ScoreAnalytics {
         this.chartContainer = document.querySelector('.chart-container');
         this.nameField = document.querySelector('.name');
         this.crossCheckId = document.querySelector('.cross-check');
+        this.deadlineTask = {};
     }
     async getList() {
         if (this.taskId == '0') {
@@ -47,7 +48,7 @@ class ScoreAnalytics {
         this.output.innerHTML = `Task name: ${taskName} \n\nResults:\n${resultsList.join('\n')} \n\nPassed amount: ${results.reduce((a,b)=>a+b,0)}\n\n`;
         this.renderCharts(labels, passed, 'taskChart');
     }
-    async myAnalytics() {
+    async myAnalytics(percentage) {
         if (this.name == '') {
             this.output.innerHTML = 'Ты кто? Напиши свой github ник выше!';
             return false;
@@ -63,6 +64,23 @@ class ScoreAnalytics {
         task.data.forEach(elem=>{
             taskList[elem.id] = elem.name;
         });
+        if (percentage) {
+            this.getTaskList(false, true).then(result=>{
+                parsedScoreList.data.content.forEach(elem=>{
+                    results.push(`Name:\t${elem.githubId}\n\nRank:\t${elem.rank}\n\nScore:\t${elem.totalScore}\n\n`);
+                    elem.taskResults.forEach(elem=>{
+                        let taskName = taskList[elem.courseTaskId];
+                        labels.push(taskName);
+                        passed.push((elem.score /result[elem.courseTaskId])*100);
+                        results.push(`${elem.courseTaskId}\t${taskName}: ${elem.score}/${result[elem.courseTaskId]}\n`);
+                    });
+                }); 
+                console.log(passed);
+                this.renderCharts(labels, passed, 'myChart', percentage);
+                this.output.innerHTML += results.join('');
+            });
+            return 'ok';
+        }
         parsedScoreList.data.content.forEach(elem=>{
             results.push(`Name:\t${elem.githubId}\n\nRank:\t${elem.rank}\n\nScore:\t${elem.totalScore}\n\n`);
             elem.taskResults.forEach(elem=>{
@@ -72,16 +90,17 @@ class ScoreAnalytics {
                 results.push(`${elem.courseTaskId}\t${taskName}: ${elem.score}\n`);
             });
         });
-        this.renderCharts(labels, passed, 'myChart');
+        console.log(passed)
+        this.renderCharts(labels, passed, 'myChart', percentage);
         this.output.innerHTML += results.join('');
-    }
-    renderCharts(labels, passed, canvasId) {
+        }
+    renderCharts(labels, passed, canvasId, percentage) {
             
         const data = {
             labels: labels,
             datasets: [{
                 indexAxis: 'y',
-                label: 'Passed',
+                label: percentage ? '% of maximum score' : 'Passed',
                 backgroundColor: 'rgb(255, 99, 132)',
                 borderColor: 'rgb(255, 99, 132)',
                 data: passed
@@ -100,11 +119,12 @@ class ScoreAnalytics {
             config
         );
     }
-    async getTaskList() {
+    async getTaskList(deadline, percentage) {
         const taskRequest = await fetch('https://app.rs.school/api/course/36/tasks'),
               task = await taskRequest.json();
         let   taskList = {},
-              crossCheckList = {};
+              crossCheckList = {},
+              deadlineList = {};
         if (task.message === "Unauthorized") {
             this.output.classList.add('output-fill');
             this.output.innerHTML = 'Зайди в app.rs.school';
@@ -115,7 +135,20 @@ class ScoreAnalytics {
             if (elem.checker === 'crossCheck') {
                 crossCheckList[elem.id] = [elem.name,elem.maxScore];
             }
+            if (deadline && new Date(elem.studentEndDate) > new Date() && new Date(elem.studentStartDate) < new Date()) {
+                deadlineList[elem.name] = [`${new Date(elem.studentEndDate).toLocaleString()}\n${new Date(elem.studentEndDate).toString().match(/(GMT+[+]+[0-9 (a-zA-Zа-яА-Я,)]+)/)[0]}`,
+                elem.descriptionUrl, elem.maxScore, `deadline submit`, elem.scoreWeight];
+            }
+            if (percentage) {
+                taskList[elem.id] = elem.maxScore;
+            }
         });
+        if (deadline) {
+            return deadlineList;
+        }
+        if (percentage) {
+            return taskList;
+        }
         for (let [key,taskName] of Object.entries(taskList)) {
             let option = document.createElement('option');
             option.value = key;
@@ -153,6 +186,22 @@ class ScoreAnalytics {
         });
         this.crossCheckId.selectedIndex = 0;
     }
+    getDeadlineList() {
+        this.getTaskList(true).then(result => {
+            this.output.innerHTML = '';
+            for (let [taskName,[date, url, max, type,scoreWeight]] of Object.entries(result)) {
+                let task = `
+                    <div class="task">
+                        <div class="task-type">Type: <span class="type">${type}</span></div>
+                        <div class="task-name">Name: <a href="${url}">${taskName}</a></div>
+                        <div class="score-wrapper"><div class="score">Score: ${max}</div><div class="score-weight">Weight: ${scoreWeight}</div></div>
+                        <div class="comment">Date: ${date}</div>
+                    </div>
+                    `
+                this.output.innerHTML +=task;
+            }
+        }).catch(reject => this.output.innerHTML = 'Дедлайнов нет');
+    }
 }
 
 new ScoreAnalytics().getTaskList();
@@ -179,7 +228,7 @@ document.querySelector('.my-analytics').addEventListener('click', ()=>{
     output.innerHTML = '';
     output.classList.add('output-fill');
     chartContainer.innerHTML = '<canvas id="myChart"></canvas>';
-    new ScoreAnalytics(id, ghName).myAnalytics().catch(()=>{output.innerHTML = 'Зайди в app.rs.school'});
+    new ScoreAnalytics(id, ghName).myAnalytics(true).catch(()=>{output.innerHTML = 'Зайди в app.rs.school'});
 });
 document.querySelector('.task-analitycs').addEventListener('click', ()=>{
     const id = document.querySelector('.id').value,
@@ -203,5 +252,15 @@ document.querySelector('.cross-check').addEventListener('change', ()=>{
     chartContainer.innerHTML = '';
     new ScoreAnalytics(id, ghName).getFeedback().catch(()=>{output.innerHTML = 'Зайди в app.rs.school или укажи СВОЙ гитхаб, хитрый'});
 });
+document.querySelector('.deadline').addEventListener('click', () => {
+    const id = document.querySelector('.id').value,
+          ghName = document.querySelector('.name').value,
+          output =document.querySelector('.output'),
+          chartContainer = document.querySelector('.chart-container');
 
+    output.innerHTML = 'LOADING...';
+    output.classList.add('output-fill');
+    chartContainer.innerHTML = '';
+    new ScoreAnalytics(id, ghName).getDeadlineList();
+});
 
